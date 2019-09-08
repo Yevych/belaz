ea731723d9b977ca4b3a355a55dc4a104511c321
Yevych <bohdan.yevych@gmail.com>
2019-09-08 11:49:33 +0300

    add warehouse section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+  //Warehouse slider
+  $('.warehouse__slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.warehouse__prev'),
+    nextArrow: $('.warehouse__next'),
+  })
 });
